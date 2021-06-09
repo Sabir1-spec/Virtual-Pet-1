abc83577a8fac0e0092040a5944c790608d059b3
RichaAgnihotri <68606897+RichaAgnihotri@users.noreply.github.com>
2021-06-09 23:45:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,20 +25,37 @@ function setup() {
 
 
 function draw() {  
-  background("black");
-
+  background("green");
+ 
+	if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
   
   drawSprites();
   //add styles here
-
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
 }
 
-function readStock() {
-
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
 }
 
-function showError() {
-  
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
 }
 
 
